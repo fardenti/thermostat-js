724c78e36ae0af480450f18bef5fe60cc22ba958
kierepkae <kierepkaedyta@gmail.com>
2019-08-21 13:04:25 +0100

    add up and down functions

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,20 +1,27 @@
-'use strict'
+"use strict";
 
-describe('Thermostat', function(){
-  
-    var thermostat;
-    
-    beforeEach(function(){
-       thermostat = new Thermostat();
-    });
+describe("Thermostat", function() {
+  var thermostat;
 
-    it('starts at 20 degrees', function(){
-        expect(thermostat.temperature()).toEqual(20);
-    });
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
 
-    it('increases the temperature with up function', function(){
-        thermostat.up();
-        expect(thermostat.temperature()).toEqual(21);
-    });
+  it("starts at 20 degrees", function() {
+    expect(thermostat.temperature()).toEqual(20);
+  });
+
+  it("increases the temperature with up function", function() {
+    thermostat.up();
+    expect(thermostat.temperature()).toEqual(21);
+  });
+  it("descreases the temperature with down function", function() {
+    thermostat.down();
+    expect(thermostat.temperature()).toEqual(19);
+  });
+  it("minimum temperature is 10 degrees", function() {
+    expect(function() {
+      thermostat.down(15);
+    }).toThrowError("default temperature is 10");
+  });
 });
-
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,18 +1,19 @@
-'use strict'
+"use strict";
 
-function Thermostat(){
+function Thermostat() {
+  this._temperature = 20;
+}
 
-    this._temperature = 20
+Thermostat.prototype.temperature = function() {
+  return this._temperature;
 };
 
-Thermostat.prototype.temperature = function(){
-    return this._temperature;
+Thermostat.prototype.up = function(number = 1) {
+  this._temperature += number;
 };
-
-Thermostat.prototype.up = function(){
-    this._temperature++;
+Thermostat.prototype.down = function(number = 1) {
+  if (this._temperature - number < 10) {
+    throw new Error("default temperature is 10");
+  }
+  this._temperature--;
 };
-
-
-
-
